3f1f6bcdd471144334e051221fcd5dea33ddf2f7
JIVC <JIVC@DESKTOP-OI1DMP2>
2022-02-02 23:12:50 -0600

    [Ejemplo V43_Serie]

---
+++ src/v43_serie/V43_Serie.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package v43_serie;
+
+/**
+ *
+ * @author JIVC
+ */
+public class V43_Serie {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int iterator=0;
+        int valor=1;
+        
+        while(iterator<50){
+            System.out.println(valor);
+            iterator++;
+            valor+=1;
+        }
+    }
+    
+}
